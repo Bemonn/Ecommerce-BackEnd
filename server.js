911e84610cc315a337fcf9479619761f3d8c8292
Bemon <tristan2107b@gmail.com>
2023-08-23 11:04:35 +1000

    Added basic code and comments as well as installed dependencies

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const routes = require('./routes');
-// import sequelize connection
+require('dotenv').config();
+
+//import sequelize connection
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//Routes
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+//sync sequelize models to the database, then turn on the server
+sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
 });
